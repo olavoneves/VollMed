232e48fc2fb5ffd88911235404596a3257c47a00
Olavo Neves <152447074+olavoneves@users.noreply.github.com>
2025-06-23 16:11:47 -0300

    Adiciona Repository e Service do Usuario

---
+++ vollmed/src/main/java/br/com/vollmed/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package br.com.vollmed.repository;
+
+import br.com.vollmed.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    UserDetails findByLogin(String login);
+}
